d97ed0faf5f08aa6b54adabdcf37825bfcc7062d
Jason Katsampes <jason.katsampes@plumpop.com>
2022-04-19 13:25:43 -0500

    Removed the Apple app store buttons. Updated the Google Play store URL.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,12 @@
-const IOS_USER_AGENT_REGEX = /ipad|iphone|ipod/i;
-const PLUMPOP_APP_STORE_URL = "";
-
 const ANDROID_USER_AGENT_REGEX = /android/i;
-const PLUMPOP_GOOGLE_PLAY_URL = "";
+const PLUMPOP_GOOGLE_PLAY_URL = "https://play.google.com/store/apps/details?id=com.plumpop.android.consumer";
 
 function initDownloadAppButtonSmallScreen() {
     let showButton = false;
     const downloadAppButton = document.querySelector("a.download-app-small");
 
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    if (IOS_USER_AGENT_REGEX.test(userAgent) && !window.MSStream) {
-        downloadAppButton.href = PLUMPOP_APP_STORE_URL;
-        showButton = true;
-    } else if (ANDROID_USER_AGENT_REGEX.test(userAgent)) {
+    if (ANDROID_USER_AGENT_REGEX.test(userAgent)) {
         downloadAppButton.href = PLUMPOP_GOOGLE_PLAY_URL;
         showButton = true;
     }
@@ -23,9 +17,6 @@ function initDownloadAppButtonSmallScreen() {
 }
 
 function initDownloadAppButtonsMediumScreen() {
-    const downloadIOSAppButton = document.querySelector("a#download-ios-app-medium");
-    downloadIOSAppButton.href = PLUMPOP_APP_STORE_URL;
-
     const downloadAndroidAppButton = document.querySelector("a#download-android-app-medium");
     downloadAndroidAppButton.href = PLUMPOP_GOOGLE_PLAY_URL;
 }
