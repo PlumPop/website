8a191bb890f2855524ef68e3333497f5aef53d01
Jason Katsampes <jason.katsampes@plumpop.com>
2022-04-30 17:21:20 -0500

    Added the Apple app store buttons and URL.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+const IOS_USER_AGENT_REGEX = /ipad|iphone|ipod/i;
+const PLUMPOP_APP_STORE_URL = "https://apps.apple.com/us/app/plumpop/id1440413166";
+
 const ANDROID_USER_AGENT_REGEX = /android/i;
 const PLUMPOP_GOOGLE_PLAY_URL = "https://play.google.com/store/apps/details?id=com.plumpop.android.consumer";
 
@@ -6,7 +9,10 @@ function initDownloadAppButtonSmallScreen() {
     const downloadAppButton = document.querySelector("a.download-app-small");
 
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    if (ANDROID_USER_AGENT_REGEX.test(userAgent)) {
+    if (IOS_USER_AGENT_REGEX.test(userAgent) && !window.MSStream) {
+        downloadAppButton.href = PLUMPOP_APP_STORE_URL;
+        showButton = true;
+    } else if (ANDROID_USER_AGENT_REGEX.test(userAgent)) {
         downloadAppButton.href = PLUMPOP_GOOGLE_PLAY_URL;
         showButton = true;
     }
@@ -17,6 +23,9 @@ function initDownloadAppButtonSmallScreen() {
 }
 
 function initDownloadAppButtonsMediumScreen() {
+    const downloadIOSAppButton = document.querySelector("a#download-ios-app-medium");
+    downloadIOSAppButton.href = PLUMPOP_APP_STORE_URL;
+
     const downloadAndroidAppButton = document.querySelector("a#download-android-app-medium");
     downloadAndroidAppButton.href = PLUMPOP_GOOGLE_PLAY_URL;
 }
